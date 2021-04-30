65e381e626a15e4efaa9c36e0d59d18a8b089f9b
Vivian Li <46733564+vivvvli@users.noreply.github.com>
2021-04-30 10:50:51 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+
+def readme():
+    """Import the README.md Markdown file and try to convert it to RST format."""
+    try:
+        import pypandoc
+        return pypandoc.convert('README.md', 'rst')
+    except(IOError, ImportError):
+        with open('README.md') as readme_file:
+            return readme_file.read()
+
+
+setup(
+    name='titanic',
+    version='0.1',
+    description='Analysis of the Evolution of Music',
+    long_description=readme(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    url='https://github.com/<vivvvli>/evolution-of-music',
+    author='Vivian Li', 
+    author_email='vivian.li982@gmail.com',  
+    license='MIT',
+    packages=['titanic'],
+)
