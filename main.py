39231fdb758566fe7d3d94f2a7c59b3456d95590
vivvv.li <vivian.li982@gmail.com>
2021-05-01 19:33:33 -0400

    new scraper for billboard hits

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ import pandas as pd
 import time
 import os
 
-client_id = os.environ.get('CLIENT_ID')
-client_secret = os.environ.get('CLIENT_SECRET')
+client_id = os.environ.get('SPOTIFY_CLIENT_ID')
+client_secret = os.environ.get('SPOTIFY_CLIENT_SECRET')
 
 client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-
-
